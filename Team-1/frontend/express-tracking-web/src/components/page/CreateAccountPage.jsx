2acb2f0fbb531c3ad85c6d56f827df865f2ac2b9
#3apaa <123915006+Lhagvaa0102@users.noreply.github.com>
2024-09-25 13:22:11 +0800

    Backend ajillgaatai bolgoj frontendteo holbov (#16)

--- Team-1/frontend/express-tracking-web/src/components/page/CreateAccountPage.jsx
+++ Team-1/frontend/express-tracking-web/src/components/page/CreateAccountPage.jsx
@@ -1,37 +1,74 @@
+"use client";
 import Link from "next/link";
 import { LogoIcon } from "../icons/LogoIcon";
 
 export const CreateAccountPage = () => {
+  const BACKEND_ENDPOINT = "http://localhost:2222/sign-up";
+
+  const handleOnSubmit = async (event) => {
+    event.preventDefault();
+
+    const userData = {
+      name: event.target.name.value,
+      password: event.target.password.value,
+      email: event.target.email.value,
+    };
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    };
+    const response = await fetch(BACKEND_ENDPOINT, options);
+    const data = await response.json();
+    console.log(data);
+    alert("Burtgelgui Hereglech");
+  };
+
   return (
     <div className="flex ">
       <div className="w-[55%] h-screen flex items-center justify-center ">
-        <div className="w-[384px] h-[426px] flex flex-col gap-3 justify-center items-center ">
+        <div className="w-[384px]  flex flex-col gap-3 justify-center items-center ">
           <LogoIcon />
           <h1 className="font-semibold text-2xl">Create Geld Account</h1>
           <p>Sign up below to create your Wallet account</p>
-          <input
-            className="h-[48px] w-full border rounded pl-3 bg-[#F3F4F6] "
-            placeholder="Name"
-            type="text"
-          />
-          <input
-            className="h-[48px] w-full border rounded pl-3 bg-[#F3F4F6] "
-            placeholder="Email"
-            type="text"
-          />
-          <input
-            className="h-[48px] w-full border rounded pl-3 bg-[#F3F4F6] "
-            placeholder="Password"
-            type="password"
-          />
-          <input
-            className="h-[48px] w-full border rounded pl-3 bg-[#F3F4F6] "
-            placeholder="Re-Password"
-            type="password"
-          />
-          <button className="btn w-full rounded-3xl text-white bg-[#0166FF] ">
-            Sign up
-          </button>
+          <form
+            className="flex flex-col gap-3 w-full"
+            onSubmit={handleOnSubmit}
+            action=""
+          >
+            <input
+              className="h-[48px] w-full border rounded pl-3 bg-[#F3F4F6] "
+              placeholder="Name"
+              type="text"
+              name="name"
+            />
+            <input
+              className="h-[48px] w-full border rounded pl-3 bg-[#F3F4F6] "
+              placeholder="Email"
+              type="text"
+              name="email"
+            />
+            <input
+              className="h-[48px] w-full border rounded pl-3 bg-[#F3F4F6] "
+              placeholder="Password"
+              type="password"
+              name="password"
+            />
+            <input
+              className="h-[48px] w-full border rounded pl-3 bg-[#F3F4F6] "
+              placeholder="Re-Password"
+              type="password"
+              name="password"
+            />
+
+            <button className="btn w-full rounded-3xl text-white bg-[#0166FF] ">
+              Sign up
+            </button>
+          </form>
+
           <div className="flex gap-3">
             <p>Already have account?</p>
             <Link className="text-[#0166FF]" href={"/"}>
--- Team-1/frontend/express-tracking-web/src/components/page/page.jsx
+++ Team-1/frontend/express-tracking-web/src/components/page/page.jsx
@@ -1,7 +1,29 @@
+"use client";
 import Link from "next/link";
 import { LogoIcon } from "../icons/LogoIcon";
 
 export const HomePage = () => {
+  const BACKEND_ENDPOINT = "http://localhost:2222/log-in";
+
+  const handleOnSubmit = async (event) => {
+    event.preventDefault();
+
+    const userData = {
+      email: event.target.email.value,
+      password: event.target.password.value,
+    };
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    };
+    const response = await fetch(BACKEND_ENDPOINT, options);
+    const data = await response.json();
+    console.log(data);
+  };
   return (
     <div className="flex ">
       <div className="w-[55%] h-screen flex items-center justify-center ">
@@ -9,22 +31,31 @@ export const HomePage = () => {
           <LogoIcon />
           <h1 className="font-semibold text-2xl">Welcome back</h1>
           <p>Welcome back, Please enter your details</p>
-          <input
-            className="h-[48px] w-full border rounded pl-3 bg-[#F3F4F6] "
-            placeholder="Email"
-            type="text"
-          />
-          <input
-            className="h-[48px] w-full border rounded pl-3 bg-[#F3F4F6] "
-            placeholder="Password"
-            type="password"
-          />
-          <button className="btn w-full rounded-3xl text-white bg-[#0166FF] ">
-            Log In
-          </button>
+          <form
+            className="flex gap-3 flex-col w-full"
+            action=""
+            onSubmit={handleOnSubmit}
+          >
+            <input
+              className="h-[48px] w-full border rounded pl-3 bg-[#F3F4F6] "
+              placeholder="Email"
+              type="text"
+              name="email"
+            />
+            <input
+              className="h-[48px] w-full border rounded pl-3 bg-[#F3F4F6] "
+              placeholder="Password"
+              type="password"
+              name="password"
+            />
+            <button className="btn w-full rounded-3xl text-white bg-[#0166FF] ">
+              Log In
+            </button>
+          </form>
+
           <div className="flex gap-3">
             <p>Don't have account?</p>
-            <Link className="text-[#0166FF]" href={"/create"}>
+            <Link className="text-[#0166FF]" href={"./sign-up"}>
               Sign up
             </Link>
           </div>
