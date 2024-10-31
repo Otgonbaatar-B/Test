15b21ad35125a4b7cf28dea4fc7448f4a2a65b63
Lhagvaa0102 <lhagvaa455@gmail.com>
2024-10-31 11:15:09 +0800

    Backend server created

---
+++ Desktop/test-project/backend/server.js
@@ -0,0 +1,9 @@
+// import http, { createServer } from "http";
+// const server = createServer((request, response) => {
+//   response.end("helloworld");
+// });
+// server.listen(2222);
+import express from "express";
+const app = express();
+
+app.listen(2222);
